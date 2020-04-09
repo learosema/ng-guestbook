efbaf31406ff0af16d31f2156554adc39168ccc1
Lea Rosema <lea.rosema@sinnerschrader.com>
2020-04-09 08:14:21 +0200

    fix: unsubscribe

--- packages/frontend/src/app/app.component.ts
+++ packages/frontend/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { GuestbookService, GuestbookPost } from './guestbook.service';
 import { Observable, Subscription } from 'rxjs';
 
@@ -7,12 +7,16 @@ import { Observable, Subscription } from 'rxjs';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less'],
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy {
   title = 'frontend';
   posts: Observable<GuestbookPost[]> = this.guestbook.getPosts();
   private subscription: Subscription = new Subscription();
 
   constructor(private guestbook: GuestbookService) {}
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
 
   submitted(post: GuestbookPost) {
     this.subscription.add(
